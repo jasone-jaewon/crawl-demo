460644a060b13bfb958a8fbbdf45dbebfa206db9
jaewon.jung <jgd0948@naver.com>
2023-01-10 17:50:31 +0900

    feat: 문자열 중복 제거 util 추가

--- src/test/java/com/hyundai/crawldemo/domain/crawl/util/StringUtilTest.java
+++ src/test/java/com/hyundai/crawldemo/domain/crawl/util/StringUtilTest.java
@@ -22,7 +22,6 @@ class StringUtilTest {
     assertThat(extracted).isEqualTo(expected);
   }
 
-
   @Test
   @DisplayName("알파벳 숫자 추출 test - null")
   public void extractAlphanumericTest_null() throws Exception {
@@ -34,5 +33,32 @@ class StringUtilTest {
 
     // then
     assertThat(extracted).isEqualTo(input);
+  }
+
+  @Test
+  @DisplayName("문자열 중복 제거 test")
+  public void removeDuplicateTest() throws Exception {
+    // given
+    String input = "cc051123aaf22f44ff11eeegg";
+    String expected = "c05123af4eg";
+
+    // when
+    String result = StringUtil.removeDuplicate(input);
+
+    // then
+    assertThat(result).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("문자열 중복 제거 test - null")
+  public void removeDuplicateTest_null() throws Exception {
+    // given
+    String input = null;
+
+    // when
+    String result = StringUtil.removeDuplicate(input);
+
+    // then
+    assertThat(result).isEqualTo(null);
   }
 }
