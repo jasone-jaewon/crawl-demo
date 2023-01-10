e5f65a7a38f2a5511139a8999a36a7843986f5d7
jaewon.jung <jgd0948@naver.com>
2023-01-10 16:43:24 +0900

    feat: html 크롤링 병렬처리 로직 추가

---
+++ src/main/java/com/hyundai/crawldemo/domain/crawl/constant/CrawlConstant.java
@@ -0,0 +1,13 @@
+package com.hyundai.crawldemo.domain.crawl.constant;
+
+import java.util.List;
+
+public class CrawlConstant {
+
+  public static final String KIA_URL = "https://www.kia.com";
+  public static final String HYUNDAI_SHOP_URL = "https://shop.hyundai.com";
+  public static final String GENESIS_URL = "https://www.genesis.com";
+  public static final List<String> CRAWL_URLS = List.of(KIA_URL, HYUNDAI_SHOP_URL, GENESIS_URL);
+
+
+}
--- src/test/java/com/hyundai/crawldemo/domain/crawl/impl/CrawlServiceImplTest.java
+++ src/test/java/com/hyundai/crawldemo/domain/crawl/impl/CrawlServiceImplTest.java
@@ -3,8 +3,10 @@ package com.hyundai.crawldemo.domain.crawl.impl;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.hyundai.crawldemo.domain.crawl.constant.CrawlConstant;
 import com.hyundai.crawldemo.domain.crawl.exception.CrawlFailException;
 import com.hyundai.crawldemo.domain.crawl.port.CrawlService;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +39,30 @@ class CrawlServiceImplTest {
 
     // when & then
     assertThrows(CrawlFailException.class, () -> crawlService.crawl(url));
+  }
+
+  @Test
+  @DisplayName("병렬 크롤링 test")
+  public void crawlConcurrentlyTest() throws Exception {
+    // given
+    List<String> crawlUrls = CrawlConstant.CRAWL_URLS;
+
+    // when
+    List<String> htmlList = crawlService.crawlByUrls(crawlUrls);
+
+    // then
+    assertThat(crawlUrls.size()).isEqualTo(htmlList.size());
+    htmlList.forEach(html -> assertThat(html).isNotBlank());
+  }
+
+  @Test
+  @DisplayName("병렬 크롤링 test - 크롤링 실패한 경우")
+  public void crawlConcurrentlyTest_includeInvalidUrl() throws Exception {
+    // given
+    String invalidUrl = "https://www.nonexistent.qqq.front";
+    List<String> crawlUrls = List.of(CrawlConstant.KIA_URL, invalidUrl);
+
+    // when & then
+    assertThrows(CrawlFailException.class, () -> crawlService.crawlByUrls(crawlUrls));
   }
 }
