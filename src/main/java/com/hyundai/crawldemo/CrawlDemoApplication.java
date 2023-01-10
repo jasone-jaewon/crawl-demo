947cb019755ed6050284530b195b9c4181f6354f
jasone-jaewon <jgd0948@naver.com>
2023-01-10 10:30:16 +0900

    Initial commit

---
+++ src/main/java/com/hyundai/crawldemo/CrawlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hyundai.crawldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyundai/crawldemo/CrawlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyundai.crawldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
