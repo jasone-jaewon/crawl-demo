5faaeee6569db4a9b4925084d4cbc34995fe4899
jaewon.jung <jgd0948@naver.com>
2023-01-12 19:51:14 +0900

    feat: spring retry config 추가

---
+++ src/main/java/com/hyundai/crawldemo/adapter/retry/config/RetryConfig.java
@@ -0,0 +1,10 @@
+package com.hyundai.crawldemo.adapter.retry.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+
+}
